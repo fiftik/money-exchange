df16509e4cf1ee96bd6b3e94ae4b63778e3d27bb
fiftik <ripthebest@gmail.com>
2019-02-21 00:11:48 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let answer = {};
+    if(currency > 10000){
+        return { error: "You are rich, my friend! We don\'t have so much coins for exchange" };
+    } else if (currency <= 0) {
+        return answer;
+    }
+
+    let countH = currency/50;
+    let countQ = (currency - Math.floor(countH)*50)/25;
+    let countD = (currency - Math.floor(countH)*50 - Math.floor(countQ)*25)/10;
+    let countN = (currency - Math.floor(countH)*50 - Math.floor(countQ)*25 - Math.floor(countD)*10)/5;
+    let countP = currency - Math.floor(countH)*50 - Math.floor(countQ)*25 - Math.floor(countD)*10 - Math.floor(countN)*5;
+
+    if(Math.floor(countH) >= 1){
+        answer.H = Math.floor(countH);
+    }
+
+    if(Math.floor(countQ) >= 1){
+        answer.Q = Math.floor(countQ);
+    }
+
+    if(Math.floor(countD) >= 1){
+        answer.D = Math.floor(countD);
+    }
+
+    if(Math.floor(countN) >= 1){
+        answer.N = Math.floor(countN);
+    }
+
+    if(Math.floor(countP) >= 1){
+        answer.P = countP;
+    }
+
+    return answer;
 }
